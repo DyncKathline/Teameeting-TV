61bc8cb7793bc7c9d1aa989f2433b5617b17ff11
Kathline <1350624667@qq.com>
2016-04-12 16:09:44 +0800

    优化界面，解决了MeetingFragment布局中光标在数字键盘上移动时ListView item选中高亮的问题

--- app/src/main/java/org/dync/tv/teameeting/adapter/roomListAdapter.java
+++ app/src/main/java/org/dync/tv/teameeting/adapter/roomListAdapter.java
@@ -1,6 +1,7 @@
 package org.dync.tv.teameeting.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,19 +11,26 @@ import android.widget.TextView;
 import org.dync.tv.teameeting.R;
 import org.dync.tv.teameeting.bean.MeetingListEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Xiao_Bailong on 2016/4/5.
  */
-public class RoomListAdapter extends BaseAdapter {
+public class roomListAdapter extends BaseAdapter {
 
     private List<MeetingListEntity> meetingLists;
     private Context context;
+    private boolean hasFocus;
 
-    public RoomListAdapter(List<MeetingListEntity> meetingLists, Context context) {
+    public roomListAdapter(List<MeetingListEntity> meetingLists, Context context) {
         this.meetingLists = meetingLists;
         this.context = context;
+    }
+
+    public void hasFocus(boolean hasFocus){
+        this.hasFocus = hasFocus;
+        notifyDataSetChanged();
     }
 
     @Override
@@ -43,11 +51,33 @@ public class RoomListAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         MeetingListEntity meetingListEntity = meetingLists.get(position);
-        View view = LayoutInflater.from(context).inflate(R.layout.item_room_list,null);
-        TextView txtRoomNum = (TextView) view.findViewById(R.id.txt_room_num);
-        TextView txtRoomName = (TextView) view.findViewById(R.id.txt_room_name);
-        txtRoomNum.setText(meetingListEntity.getMeetingid());
-        txtRoomName.setText(meetingListEntity.getMeetname());
-        return view;
+        HolderView holderView = null;
+        if (convertView == null) {
+            holderView = new HolderView();
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_room_list, null);
+            holderView.txtRoomNum = (TextView) convertView.findViewById(R.id.txt_room_num);
+            holderView.txtRoomName = (TextView) convertView.findViewById(R.id.txt_room_name);
+            holderView.txtRoomNum.setText(meetingListEntity.getMeetingid());
+            holderView.txtRoomName.setText(meetingListEntity.getMeetname());
+
+            convertView.setTag(holderView);
+        } else {
+            holderView = (HolderView) convertView.getTag();
+        }
+
+        if (hasFocus) {
+            convertView.setBackgroundResource(R.drawable.selector_item);
+            Log.e("BaseAdapter", "getView得到焦点====" + position);
+        } else {
+            convertView.setBackgroundResource(R.drawable.button_default);
+            Log.e("BaseAdapter", "getView失去焦点====" + position);
+        }
+
+        return convertView;
+    }
+
+    private class HolderView {
+        public TextView txtRoomNum;
+        public TextView txtRoomName;
     }
 }
